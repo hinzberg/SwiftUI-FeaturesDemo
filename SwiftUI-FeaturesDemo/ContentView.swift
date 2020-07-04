acbf6ab6d684f57a3d8e4f4c1f9d1b94aaaa9f9e
Holger Hinzberg <holger@hinzberg.de>
2020-07-04 10:36:11 +0200

    Initial Commit

---
+++ SwiftUI-FeaturesDemo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUI-FeaturesDemo
+//
+//  Created by Holger Hinzberg on 04.07.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-FeaturesDemo/SwiftUI_FeaturesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FeaturesDemoApp.swift
+//  SwiftUI-FeaturesDemo
+//
+//  Created by Holger Hinzberg on 04.07.20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FeaturesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
